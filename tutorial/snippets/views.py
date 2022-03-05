684f8d789553c9f608bc63b0d9bdd248fd5044e0
noaholatoye <noaholatoye@gmail.com>
2022-03-05 20:50:06 +0100

    Tutorial 5: Relationships & Hyperlinked APIs

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,10 +1,18 @@
-from snippets.models import Snippet
+from .models import Snippet
 from django.contrib.auth.models import User
 from .serializers import SnippetSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly
 from rest_framework import generics
 from rest_framework import permissions
 
+# Function view
+# Creating an endpoint for the root of our API
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+# Creating an endpoint for the highlighted snippets
+from rest_framework import renderers
 
 class SnippetList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
@@ -31,3 +39,22 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+# Creating an endpoint for the root of our API
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
+# Creating an endpoint for the highlighted snippets
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
